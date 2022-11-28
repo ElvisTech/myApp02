113a9f2c4ea8faa1d91374d2aecf04f6fbd78d81
ElvisTech <elvis_tech@hotmail.com>
2022-11-28 19:48:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html xml:lang="en">
-<head>
-  <title>HTML Elements Reference</title>
-</head>
-<body>
-<h1> Hello World </h1>
-prueba desde git, con revision de sonar hasta deploy
-  </body>
-  </html>
+
+<h1> Hola Daniel </h1>
+prueba desde git, con revision de sonar hasta deploy en apache
+
